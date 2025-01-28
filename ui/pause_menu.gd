db5281b93faa600e334252aa63882eccf269bfeb
Selaries Raphael <raphael.selaries@ensaama.net>
2025-01-28 21:39:25 +0100

    Fixes

---
+++ ui/pause_menu.gd
@@ -0,0 +1,32 @@
+extends Control
+
+
+@export var main_menu_scene: PackedScene
+
+
+func _ready() -> void:
+	visible = false
+
+
+func _on_reload_button_pressed() -> void:
+	#get_tree().change_scene_to_packed(main_menu_scene)
+	get_tree().change_scene_to_file("res://ui/main_menu.tscn")
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		visible = !visible
+		
+		if visible == false:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+		else:
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
+
+func _on_resume_button_pressed() -> void:
+	visible = false
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
